9e0522b539bab1782a42d9e13e6b06a9c526e17c
Karl <growllcraft@gmail.com>
2023-01-14 17:27:38 +0800

    Change Team Name

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,20 +5,20 @@ public class Main {
 
 
 	public static void main(String[] args) throws InterruptedException {
-		GameCharacter c1 = new GameCharacter(CharacterClass.HEALER,2);
+		GameCharacter c1 = new GameCharacter(CharacterClass.HEALER,20);
 		GameCharacter c2 = new GameCharacter(CharacterClass.MAGE,1);
 		GameCharacter c3 = new GameCharacter(CharacterClass.WARRIOR,1);
 
 		GameCharacter[] t1 = {c1,c2,c3};
 
-		GameCharacter d1 = new GameCharacter(CharacterClass.HEALER,2);
+		GameCharacter d1 = new GameCharacter(CharacterClass.HEALER,20);
 		GameCharacter d2 = new GameCharacter(CharacterClass.MAGE,1);
 		GameCharacter d3 = new GameCharacter(CharacterClass.WARRIOR,1);
 
 		GameCharacter[] t2 = {d1,d2,d3};
 
-		Team team1 = new Team(t1, "ECHO");
-		Team team2 = new Team(t2, "BLACKLIST");
+		Team team1 = new Team(t1, "Karl");
+		Team team2 = new Team(t2, "Carlo");
 
 		team1.gotoBattle(team2);
 		team2.gotoBattle(team1);
